7ae358c7cffd9ec8a24931037dfac6557911a78a
Gabriel Bergoc <gabriel_cb17@hotmail.com>
2021-04-15 15:32:04 -0300

    added B - functioning webserver

--- johannsebastianbot.py
+++ johannsebastianbot.py
@@ -1,5 +1,6 @@
 import discord
 import decouple
+import B
 from discord.ext import commands
 from discord.utils import get
 
@@ -8,7 +9,9 @@ JOHANNSEBASTIANBOT_CLIENT_SECRET = decouple.config("JOHANNSEBASTIANBOT_CLIENT_SE
 JOHANNSEBASTIANBOT_PUBLIC_KEY = decouple.config("JOHANNSEBASTIANBOT_PUBLIC_KEY")
 JOHANNSEBASTIANBOT_TOKEN = decouple.config("JOHANNSEBASTIANBOT_TOKEN")
 
-client = discord.Client()
+# client = discord.Client()
+client = commands.Bot(command_prefix="&")
+client.remove_command('help')
 
 @client.event
 async def on_ready():
@@ -31,4 +34,15 @@ async def on_member_join(member):
     await member.add_roles(role)
     print(f'{member} has joined the server.')
 
+@client.command()
+async def command(ctx):
+    await ctx.send('This command was successful!')
+
+@client.command()
+async def help(ctx):
+    embed = discord.Embed(name="Help", value="help command", color=0xFFFFFF)
+    embed.add_field(name="command", value="command description")
+    await ctx.send(embed=embed)
+
+B.b()
 client.run(JOHANNSEBASTIANBOT_TOKEN)
